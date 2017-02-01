b5a6f9ab7ed742f2454dffd061ea4c1b4e5ef92f
mblank722 <mitchellblank@sbcglobal.net>
2017-02-01 13:56:12 -0800

    view

--- apps/log_reg/views.py
+++ apps/log_reg/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render redirect
 
 # Create your views here.
 def index(request):
     return render (request, 'log_reg/index.html')
+
+def success(request):
+    return render (request, 'log_reg/success.html')
