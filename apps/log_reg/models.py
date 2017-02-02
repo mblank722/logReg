d0c2ec01eca6ce7a19da7f9824a1bd3cf8cd8d0f
mblank722 <mitchellblank@sbcglobal.net>
2017-02-02 11:58:14 -0800

    models update2

--- apps/log_reg/models.py
+++ apps/log_reg/models.py
@@ -31,7 +31,7 @@ class UserManager(models.Manager):
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=30)
+    email = models.EmailField()
     password = models.CharField(max_length=30)
     birth_day = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
